d66a3bfdbb48c0b4bc194e0245e74eb365031208
rikster-r <seitdanial43@gmail.com>
2022-09-09 18:39:30 +0600

    setup folders

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    fallback: {
+      util: require.resolve("util/"),
+    }
+  },
+  devtool: 'inline-source-map',
+};

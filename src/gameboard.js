177b09c1bd7e4d16d3dd5ffdd36b8ea4dd41eac3
rikster-r <seitdanial43@gmail.com>
2022-09-11 18:28:44 +0600

    make layout, add styles

--- src/gameboard.js
+++
@@ -1,7 +0,0 @@
-class Gameboard {
-  constructor() {
-    
-  }
-
-  
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ],
+      },
+    ],
   },
   resolve: {
     fallback: {
-      util: require.resolve("util/"),
+      util: require.resolve("util/")
     }
   },
   devtool: 'inline-source-map',
